44dbbd048b2657d0ae391fd86be22cd77a7fd28f
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-11-10 20:02:40 -0300

    feat: added argentina feature

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const cowsay = require("cowsay");
+// const cowsay = require("cowsay");
 
-console.log(cowsay.say({
-    text : "Hola MUUUUUUUUUUUUUUndo",
-    e : "UU",
-    T : "W"
-}));
+// console.log(cowsay.say({
+//     text : "Hola MUUUUUUUUUUUUUUndo",
+//     e : "UU",
+//     T : "W"
+// }));
+
+const countries = require('./countries.json');
+const argentina = countries[10];
+
+
+console.log(argentina);
