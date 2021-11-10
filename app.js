fed419484a4b6924fcae51a06338d5492ca08043
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-11-10 20:34:50 -0300

    conflicts resolve

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log(cowsay.say({
     text : "Hola MUUUUUUUUUUUUUUndo",
     e : "UU",
     T : "W"
+<<<<<<< HEAD
 }));
 
 const countries = require('./countries.json');
@@ -11,3 +12,6 @@ const argentina = countries[10];
 
 
 console.log(argentina);
+=======
+}));
+>>>>>>> parent of 44dbbd0... feat: added argentina feature
