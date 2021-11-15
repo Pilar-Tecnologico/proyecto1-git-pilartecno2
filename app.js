790a75cba3a71f8f0510e086c90b2ff3e69536e6
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-11-15 20:06:29 -0300

    cherrypick conflicts resolve

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // const cowsay = require("cowsay");
 
 // console.log(cowsay.say({
@@ -5,9 +6,22 @@
 //     e : "UU",
 //     T : "W"
 // }));
+=======
+const cowsay = require("cowsay");
+const Router = require('./routes');
+console.log(cowsay.say({
+    text : "Hola MUUUUUUUUUUUUUUndo",
+    e : "UU",
+    T : "W"
+}));
+>>>>>>> 668c3e4... routes feature
 
 const countries = require('./countries.json');
 const argentina = countries[10];
 
 
+<<<<<<< HEAD
 console.log(argentina);
+=======
+Router.route();
+>>>>>>> 668c3e4... routes feature
